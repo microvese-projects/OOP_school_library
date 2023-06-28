93e3e4c3dd992b7118d69560308806eedc1e1a1e
Benson Njuguna <njugunab655@gmail.com>
2023-06-28 17:19:10 +0300

    Add rental class and link associations

--- person.rb
+++ person.rb
@@ -2,13 +2,14 @@ require './nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
