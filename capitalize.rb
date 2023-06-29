3178db8460b37628fc5a33d01e993ee64aaab4e1
Benson Njuguna <njugunab655@gmail.com>
2023-06-29 11:14:36 +0300

    Create app.rb file

--- capitalize.rb
+++
@@ -1,8 +0,0 @@
-require './name_decorator'
-
-class CapitalizeDecorator < NameDecorator
-  def correct_name
-    name = @nameable.correct_name
-    name.capitalize
-  end
-end
--- trimname.rb
+++
@@ -1,8 +0,0 @@
-require './name_decorator'
-
-class TrimmerDecorator < NameDecorator
-  def correct_name
-    name = @nameable.correct_name
-    name[0...10]
-  end
-end
